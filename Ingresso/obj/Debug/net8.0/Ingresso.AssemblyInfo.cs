fc8e6395a1876cbd5c28002154bd8277c44f37a5
Dark1nessss <frankhorn99@gmail.com>
2024-09-28 12:04:41 +0100

    Update

--- Ingresso/obj/Debug/net8.0/Ingresso.AssemblyInfo.cs
+++ Ingresso/obj/Debug/net8.0/Ingresso.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ingresso")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df45e0164dd5c2c7250737ec79614b3140ad7e6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c7aa875da2871d4973f8dcb781448317997386c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ingresso")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ingresso")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
